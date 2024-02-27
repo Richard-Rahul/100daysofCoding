2f01fd32f1e60eaa5914b8b76ccf109f7b22f8d9
Richard Rahul <mr.thesaga@gmail.com>
2024-02-27 14:27:52 +0530

    new updates

---
+++ Day-45/test.js
@@ -0,0 +1,16 @@
+const accountId = 1316
+let accountEmail = "richard_bhaiya@gmail.com"
+var accountPassword = "Suggu@12345"
+accountCity = "Gurgaon"
+let accountState;
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+// Const - 
+
+// Let - 
+
+// Var - 
+
+// Comment in JS - 
