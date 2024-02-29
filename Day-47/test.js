85db00d8886b271c89183bd2ce11cdf1beda8813
Richard Rahul <mr.thesaga@gmail.com>
2024-02-29 14:59:09 +0530

    code updates

---
+++ Day-47/test.js
@@ -0,0 +1,17 @@
+let fullName = "Richard Rahul";
+let age = 24;
+
+console.log(fullName)
+
+typeof fullName;
+
+a = 40
+b = 20
+
+sum = a+b
+console.log(sum)
+
+c = 45
+d = 5
+sum = c+d
+console.log(sum)
