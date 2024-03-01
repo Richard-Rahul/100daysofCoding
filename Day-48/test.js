1cbaef24286e37d05353fef2da4a29101956096b
Richard Rahul <mr.thesaga@gmail.com>
2024-03-01 13:51:19 +0530

    code updates

--- Day-48/test.js
+++ Day-48/test.js
@@ -1,4 +1,4 @@
-let student = {
+const student = {
     name:"Rahul",
     age:24,
     marks:640,
@@ -7,7 +7,7 @@ let student = {
 
 console.log(student);
 
-let emp = {
+const emp = {
     name:"Geeta",
     age:24,
     marks:660,
@@ -15,3 +15,40 @@ let emp = {
 };
 
 console.log(emp)
+console.log(student["name"]);
+
+student["age"] = student["age"] + 5;
+console.log(student["age"]);
+
+student["name"] = "Geeta Verma";
+console.log(student["name"]);
+
+
+// Q1 - create a const object called "product" to store information.
+
+const product = {
+    title: "Parker Jotter Standard CT Ball Pen (Black)",
+    rating: 4.5,
+    offer: "5% off",
+    price: 270,
+    remark: "Deal of the Day",
+}
+
+console.log(product);
+
+// Q2 Create a const object called "profile" to store information.
+
+const profile = {
+    userName: "richardrahul",
+    isFollow: true,
+    isMessage: false,
+    post: "195 Posts",
+    followers: "500k followers",
+    following: "120 following",
+    fullName: "Richard Rahul",
+    isTag: "richard_rahul",
+    info: "Founder of Programmerfect | Content Creator",
+}
+
+console.log(profile);
+
