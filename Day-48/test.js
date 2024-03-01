670cda37e0aca4aedd9d4015436d0e3a81496fc2
Richard Rahul <mr.thesaga@gmail.com>
2024-03-01 12:53:46 +0530

    code updates

---
+++ Day-48/test.js
@@ -0,0 +1,17 @@
+let student = {
+    name:"Rahul",
+    age:24,
+    marks:640,
+    isPass:true,
+};
+
+console.log(student);
+
+let emp = {
+    name:"Geeta",
+    age:24,
+    marks:660,
+    isPass:true,
+};
+
+console.log(emp)
