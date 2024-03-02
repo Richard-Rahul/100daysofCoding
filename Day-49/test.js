3502c3ce8c8fea3a7698a5dad045153cf7e19825
Richard Rahul <mr.thesaga@gmail.com>
2024-03-02 16:37:31 +0530

    code updates

---
+++ Day-49/test.js
@@ -0,0 +1,17 @@
+// Declare variables
+let num1 = 10;
+let num2 = 5;
+
+// Perform arithmetic operations
+let sum = num1 + num2;
+let difference = num1 - num2;
+let product = num1 * num2;
+let quotient = num1 / num2;
+let remainder = num1 % num2;
+
+// Print results
+console.log("Sum:", sum); 
+console.log("Difference:", difference); 
+console.log("Product:", product); 
+console.log("Quotient:", quotient); 
+console.log("Remainder:", remainder); 
