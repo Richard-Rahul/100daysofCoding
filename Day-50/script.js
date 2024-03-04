3a27f27420c9a8634899ea4e3c143307965f097b
Richard Rahul <mr.thesaga@gmail.com>
2024-03-04 14:01:58 +0530

    code updates

---
+++ Day-50/script.js
@@ -0,0 +1,24 @@
+// Q1 - What is the value of age after this code runs?
+
+let age = 23;
+age + 2; // after 2 years
+
+age = age + 2;
+
+console.log(age)
+
+
+// Q2 - What is the value of avg after this code runs?
+
+let hindi = 80;
+let eng = 90;
+let math = 100;
+let avg = (hindi + eng + math) / 3;
+
+console.log(avg);
+
+// Q3 
+
+let marks = 640;
+let newMarks = ++marks;
+console.log(newMarks);
