95a49cb667793d53d15be136a6ef7cc447926ba2
Richard Rahul <mr.thesaga@gmail.com>
2024-03-04 15:37:47 +0530

    code updates

---
+++ Day-51/script.js
@@ -0,0 +1,61 @@
+// Q1 - What is the value of each variable in each line of code?
+
+/*let num = 5;
+let newNum = num++;
+newNum = ++num;
+
+console.log(newNum);*/
+
+// Q2 
+/*
+let x = 100;
+let newX = ++x;
+newX = x++;
+
+console.log(newX) */
+
+// Q3
+
+let y = 10;
+let newY = y--;
+newY = --y;
+
+console.log(newY)
+
+let num = 5;
+let newNum = num++;
+newNum = ++num;
+
+console.log(newNum);
+
+
+// Q4
+
+let x = 10;
+if (x > 5) {
+    console.log("x is greater than 5");
+}
+
+
+// Q5
+
+let temperature = 25;
+if (temperature > 30) {
+    console.log("It's hot outside");
+} else {
+    console.log("It's not too hot outside");
+}
+
+
+// Q6
+
+let grade = 85;
+if (grade >= 90) {
+    console.log("A grade");
+} else if (grade >= 80) {
+    console.log("B grade");
+} else if (grade >= 70) {
+    console.log("C grade");
+} else {
+    console.log("Fail");
+}
