8e93a1d02d6516312c27a6f682dd8b49752102d3
Richard Rahul <mr.thesaga@gmail.com>
2024-03-06 15:41:16 +0530

    Code updates

---
+++ Day-52/script.js
@@ -0,0 +1,66 @@
+// conditional statement
+
+// Q1 - Write a program that determines whether a given number is positive or not.
+
+// For Positive Number
+let num1 = 25;
+
+if (num1 >= 0) {
+    console.log("Positive Number");
+} else {
+    console.log("Negative Number");
+}
+
+// For Negative Number
+
+let num2 = -10;
+
+if (num2 >= 0) {
+    console.log("Positive Number");
+} else {
+    console.log("Negative Number");
+}
+
+
+// Q2 - Write a program that checks if a number is even or odd.
+
+// Even Number
+let number = 12;
+
+if (number % 2 === 0) {
+    console.log("Even Number");
+} else {
+    console.log("Odd Number");
+}
+
+// Odd Number
+
+let number1 = 12;
+
+if (number1 % 2 === 0) {
+    console.log("Even Number");
+} else {
+    console.log("Odd Number");
+}
+
+// Q3 - Write a program to determine the greater of two numbers.
+
+let a = 50;
+let b = 20;
+
+if (a > b) {
+    console.log("A is greater then B");
+} else {
+    console.log("A is less then B");
+}
+
+// Q4 - Votar Card
+
+let age = 12;
+
+if (age >= 18) {
+    console.log("You can vote");
+} else {
+    console.log("You cannot vote");
+}
+
