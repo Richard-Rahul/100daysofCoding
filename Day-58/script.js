735ae1862a06fa70bc124b1b2d092804402e0a29
Richard Rahul <mr.thesaga@gmail.com>
2024-03-13 13:40:50 +0530

    code updates

---
+++ Day-58/script.js
@@ -0,0 +1,41 @@
+// String Indices
+
+let fullName = "Richard Rahul";
+
+console.log(fullName[0]);
+console.log(fullName[1]);
+console.log(fullName[2]);
+console.log(fullName[3]);
+console.log(fullName[4]);
+console.log(fullName[5]);
+console.log(fullName[6]);
+console.log(fullName[7]);
+console.log(fullName[8]);
+console.log(fullName[9]);
+console.log(fullName[10]);
+console.log(fullName[11]);
+console.log(fullName[12]);
+
+console.log(fullName[100]);
+
+console.log(fullName.length);
+
+let firstName = "Richard";
+let lastName = "Rahul";
+
+let full_Name = firstName + " " + lastName;
+console.log(full_Name);
+
+let name = "Geeta";
+console.log(name.length);
+
+let first_Name = "Rahul";
+console.log(first_Name[0]);
+console.log(first_Name[4]);
+
+let a = "apnacollege";
+let b = 123;
+
+let c = a + b;
+
+console.log(c);
