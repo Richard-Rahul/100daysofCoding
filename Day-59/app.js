bbda503c263f41a85ad5e009038d2333dad9b466
Richard Rahul <mr.thesaga@gmail.com>
2024-03-14 16:45:01 +0530

    code updates

--- Day-59/app.js
+++ Day-59/app.js
@@ -18,3 +18,68 @@ console.log(a / b);
 console.log(a ** b);
 console.log(a % b);
 console.log(a**b);
+
+// String practice
+
+// String Indices
+
+let fullName = "Richard Rahul";
+
+console.log(fullName[0]);
+console.log(fullName[1]);
+console.log(fullName[2]);
+console.log(fullName[3]);
+console.log(fullName[4]);
+console.log(fullName[5]);
+console.log(fullName[6]);
+console.log(fullName[7]);
+console.log(fullName[8]);
+console.log(fullName[9]);
+console.log(fullName[10]);
+console.log(fullName[11]);
+console.log(fullName[12]);
+
+console.log(fullName[100]);
+
+console.log(fullName.length);
+
+let firstName = "Richard";
+let lastName = "Rahul";
+
+let full_Name = firstName + " " + lastName;
+console.log(full_Name);
+
+let name = "Geeta";
+console.log(name.length);
+
+let first_Name = "Rahul";
+console.log(first_Name[0]);
+console.log(first_Name[4]);
+
+let a = "apnacollege";
+let b = 123;
+
+let c = a + b;
+
+console.log(c);
+
+
+const firstname = "John";
+const lastname = "Doe";
+const fullname = firstname + " " + lastname;
+console.log(fullname);
+
+// Get the length of a string
+const message = "Hello, world!";
+const messageLength = message.length;
+console.log(messageLength);
+
+// Get the character at a specific index in a string
+const letter = message.charAt(0);
+console.log(letter);
+
+// Find the first occurrence of a substring in a string
+const index = message.indexOf("world");
+
+// Replace all occurrences of a substring in a string with another substring
+const newMessage = message.replace("world", "universe");
