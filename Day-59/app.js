bbe313ea627680f5587beaf73a701cc31cda0c28
Richard Rahul <mr.thesaga@gmail.com>
2024-03-14 14:00:26 +0530

    code updates

---
+++ Day-59/app.js
@@ -0,0 +1,19 @@
+let pencilPrice = 10;
+let erasorPrice = 5;
+
+console.log("The total price: ", pencilPrice + erasorPrice);
+
+// Template Literals
+console.log(`The total price:  ${pencilPrice + erasorPrice}`);
+
+// Arithmetic Operators
+
+let a = 40;
+let b = 10;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a ** b);
+console.log(a % b);
