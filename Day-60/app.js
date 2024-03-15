2597fcac8bf586fac3c1917db71c07017008447c
Richard Rahul <mr.thesaga@gmail.com>
2024-03-15 16:24:48 +0530

    code updates

---
+++ Day-60/app.js
@@ -0,0 +1,19 @@
+let a = "123";
+let b = 123;
+
+console.log( a === b);
+console.log( a == b);
+
+let num1 = 0;
+let num2 = " ";
+
+console.log(num1 == num2);
+console.log(num1 === num2);
+
+let x = null; //null
+let y; // undefined
+
+console.log(x==y); // compares value only, not type
+console.log(x===y); // compares both value and type
+
+
