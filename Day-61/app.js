eefddd0569fe898a8b2715375e0bdc8fb4192e92
Richard Rahul <mr.thesaga@gmail.com>
2024-03-16 14:13:12 +0530

    code updates

--- Day-61/app.js
+++ Day-61/app.js
@@ -7,3 +7,36 @@ console.log(a<b);
 console.log(a<=b);
 console.log(a==b);
 console.log(a===b);
+
+
+// if statement
+// let age = 15;
+
+// if (age>=18) {
+//     console.log("You can vote.")
+// }
+
+// Q- Create a traffic light system that shows what to do based on color.
+
+let color = "red";
+if (color === "red") {
+    console.log("Stop");
+}
+
+if (color === "yellow") {
+    console.log("Slow-Down");
+}
+
+if (color === "green") {
+    console.log("Go");
+}
+
+// else statement
+
+let age = 12;
+
+if (age >= 18) {
+    console.log("You can vote");
+} else {
+    console.log("You cannnot vote.")
+}
