58a5249ab37f296381e893cf99987bab0be3f64b
Richard Rahul <mr.thesaga@gmail.com>
2024-03-16 15:54:51 +0530

    code updates

--- Day-61/app.js
+++ Day-61/app.js
@@ -40,3 +40,48 @@ if (age >= 18) {
 } else {
     console.log("You cannnot vote.")
 }
+
+
+let marks = 25;
+
+if (marks >= 80) {
+    console.log("A");
+} else if (marks >=60) {
+    console.log("B")
+} else if (marks >= 33) {
+    console.log("C");
+} else if (marks < 33) {
+    console.log("F");
+}
+
+
+let month = "april";
+
+if (month === "january") {
+    console.log("Winter is here");
+} else if (month === "april") {
+    console.log("Summer is here");
+}
+
+let rang = "green";
+
+if (rang === "red") {
+    console.log("STOP");
+} else if (rang === "yellow") {
+    console.log("SLOW-DOWN");
+} else if (rang === "green") {
+    console.log("GO");
+}
+
+
+let my_color;
+
+if (my_color === "red") {
+    console.log("STOP");
+} else if (my_color === "yellow") {
+    console.log("SLOW-DOWN");
+} else if (my_color === "green") {
+    console.log("GO");
+} else {
+    console.log("Traffic light broken");
+}
