0f5e8014d3a642fceeedfb278d1f3ecd98ad2b79
Richard Rahul <mr.thesaga@gmail.com>
2024-03-16 16:02:31 +0530

    code updates

--- Day-61/app.js
+++ Day-61/app.js
@@ -85,3 +85,21 @@ if (my_color === "red") {
 } else {
     console.log("Traffic light broken");
 }
+
+/* Q - create a system to calculate popcorn prices based on the size customer asked for:
+   if size is "XL", price is Rs. 250
+   if size is "L", price is Rs. 200
+   if size is "M", price is Rs. 100
+   if size is "S", price is Rs. 50  */
+
+let size = "S";
+
+if (size === "XL") {
+    console.log("Price is Rs. 250");
+} else if (size === "L") {
+    console.log("Price is Rs. 200");
+} else if (size === "M") {
+    console.log("Price is Rs. 100");
+} else {
+    console.log("Price is Rs. 50");
+}
