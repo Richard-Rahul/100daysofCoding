1109bdd23ada3d4e2c22f33605de4cfe04cf9300
Richard Rahul <mr.thesaga@gmail.com>
2024-03-18 12:25:50 +0530

    code updates

---
+++ Day-63/app.js
@@ -0,0 +1,16 @@
+let str = "apple";
+
+if ((str[0] === 'a') && (str.length > 3)) {
+    console.log("Good String");
+} else {
+    console.log("Bad String");
+}
+
+
+let num = 12;
+
+if ((num%3 === 0) && (num+1 == 15) || (num-1 == 11)) {
+    console.log("Safe");
+} else {
+    console.log("Unsafe");
+}
