a66ce7407740cb72204530605a834e2bba193253
Richard Rahul <mr.thesaga@gmail.com>
2024-03-21 20:21:01 +0530

    code updates

--- Day-66/app.js
+++ Day-66/app.js
@@ -3,3 +3,8 @@ let str = "ILoveYou";
 console.log(str.indexOf("Love"));
 console.log(str.indexOf("u"));
 console.log(str.indexOf("r"));
+
+let newMsg = "    RichardRahul     ";
+
+console.log(newMsg.toLocaleUpperCase().trim());
+
