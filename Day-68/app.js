810723d206d49250ce6c2adb6625a272586ef321
Richard Rahul <mr.thesaga@gmail.com>
2024-03-23 12:57:42 +0530

    code updates

---
+++ Day-68/app.js
@@ -0,0 +1,25 @@
+let str = "Programmerfect";
+
+console.log(str.slice(0, 3));
+console.log(str.slice(10));
+console.log(str.slice(-2));
+
+
+let text = "microsoft";
+
+let newText = text.replace("microsoft", "programmerfect");
+console.log(newText);
+
+
+// let msg = "Richard Rahul";
+// console.log(msg.repeat(3));
+
+let msg = "help";
+console.log(msg.trim(), msg.toLocaleUpperCase());
+
+let name = "ApnaCollege";
+
+console.log(name.slice(4,9));
+console.log(name.indexOf("na"));
+console.log(name.replace("Apna", "Our"));
+
