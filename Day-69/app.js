6ed341bc6b45278e9d323b372d74b3df69364ca3
Richard Rahul <mr.thesaga@gmail.com>
2024-03-24 13:36:35 +0530

    code updates

---
+++ Day-69/app.js
@@ -0,0 +1,11 @@
+let marks = [99, 85, 93, 76, 62];
+let names = ["rahul", "geeta", "neha", "suggu", "aditya"];
+let info = ["aman", 25, 6.1];
+
+console.log(marks);
+console.log(names);
+console.log(info);
+
+// Array Length
+
+console.log(marks.length);
