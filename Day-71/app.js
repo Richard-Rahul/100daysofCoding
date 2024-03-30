de5df28b05abad2e2a66b8a63719595f2002e51e
Richard Rahul <mr.thesaga@gmail.com>
2024-03-30 15:18:25 +0530

    code updates

--- Day-71/app.js
+++ Day-71/app.js
@@ -10,3 +10,16 @@ let secondaryColor = ["Yellow", "Pink", "Blue"];
 // concat() - merge 2 array
 let finalColor = primaryColor.concat(secondaryColor);
 console.log(finalColor);
+console.log(primaryColor);
+console.log(secondaryColor);
+
+// reverse 
+
+primaryColor.reverse();
+console.log(primaryColor);
+
+// Slice 
+
+let colors = ["Red", "White", "Black", "Pink", "Yellow", "Blue"];
+let final = colors.slice(2, 4);
+console.log(final);
