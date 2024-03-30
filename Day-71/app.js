f669db3ae3ebe7d606f9981653e21ce47da9fd51
Richard Rahul <mr.thesaga@gmail.com>
2024-03-30 14:22:08 +0530

    code updates

--- Day-71/app.js
+++ Day-71/app.js
@@ -2,3 +2,11 @@ let names = ["Rahul", "Geeta", "Suggu", "Buggu"];
 names.includes("Geeta");   // true
 names.includes("Sandhya");  // false
 console.log(names);
+
+
+let primaryColor = ["Red", "White", "Black"];
+let secondaryColor = ["Yellow", "Pink", "Blue"];
+
+// concat() - merge 2 array
+let finalColor = primaryColor.concat(secondaryColor);
+console.log(finalColor);
