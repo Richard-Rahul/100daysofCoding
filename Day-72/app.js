f3b1202d7f129601f861c67fc40ff449ed892485
Richard Rahul <mr.thesaga@gmail.com>
2024-03-31 14:20:47 +0530

    code updates

---
+++ Day-72/app.js
@@ -0,0 +1,8 @@
+let colors = ["red", "pink", "black", "white", "blue", "yellow"];
+
+// let final = colors.slice(-3);
+// let final = colors.slice(2);
+// let final = colors.slice(2, 5);
+let final = colors.slice(5);
+
+console.log(final);
