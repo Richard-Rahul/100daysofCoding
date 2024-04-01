06a6e637217fa3db0868a56efcf122af1b25f13e
Richard Rahul <mr.thesaga@gmail.com>
2024-04-01 06:01:31 +0530

    code updates

--- Day-73/app.js
+++ Day-73/app.js
@@ -1,33 +1,33 @@
-// Generate a random number between 1 and 100
+
 const randomNumber = Math.floor(Math.random() * 100) + 1;
 
-// Prompt the user to guess a number
+
 let guess = parseInt(prompt("Guess a number between 1 and 100:"));
 
-// Counter to keep track of the number of attempts
+
 let attempts = 1;
 
-// Loop until the user guesses the correct number
+
 while (guess !== randomNumber) {
-    // Check if the guess is too low
+  
     if (guess < randomNumber) {
         alert("Too low! Try again.");
     }
-    // Check if the guess is too high
+   
     else {
         alert("Too high! Try again.");
     }
     guess = parseInt(prompt("Guess a number between 1 and 100:"));
     attempts++;
 
-    // If the user attempts more than 10 times, break out of the loop
+
     if (attempts > 10) {
         alert("You've reached the maximum number of attempts. The correct number was " + randomNumber + ".");
         break;
     }
 }
 
-// If the user guessed the correct number within 10 attempts
+
 if (guess === randomNumber) {
     alert("Congratulations! You guessed the number " + randomNumber + " correctly in " + attempts + " attempts.");
 }
