fa5a5b516a8e6d91fe930f22ec39351a1623342c
Richard Rahul <mr.thesaga@gmail.com>
2024-04-03 18:48:56 +0530

    todo app

--- Day-74/script.js
+++ Day-74/script.js
@@ -26,13 +26,14 @@ while(true) {
         let task = prompt("please enter the task you want to add");
         todo.push(task);
         console.log("task added");
+
     } else if (req == "delete") {
         let idx = prompt("please enter the task index");
         todo.splice(idx, 1);
         console.log("task deleted");
+
     } else {
         console.log("wrong request");
     }
-
     req = prompt("please enter your request");
 }
