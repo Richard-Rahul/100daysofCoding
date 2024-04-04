3faeb7be68f87e199b2139f76a821a2c989244a8
Richard Rahul <mr.thesaga@gmail.com>
2024-04-04 13:45:20 +0530

    code updates

---
+++ Day-75/app.js
@@ -0,0 +1,18 @@
+for (let i=0; i<=5; i++) {
+    console.log(i);
+}
+
+
+for (let i=10; i>=0; i--) {
+    console.log(i);
+}
+
+
+for (let i=2; i<=20; i+=2) {
+    console.log(i);
+}
+
+
+for (let i=10; i<=5; i++) {
+    console.log(i);
+}
