abbff784a59a3cb51c8ad9d6b809fa2cef4771ef
Richard Rahul <mr.thesaga@gmail.com>
2024-04-05 13:39:04 +0530

    code updates

---
+++ Day-77/app.js
@@ -0,0 +1,42 @@
+for (let i=1; i<=33; i=i+2) {
+    console.log(i);
+}
+
+
+// backward
+for (let i=15; i>=1; i=i-2) {
+    console.log(i);
+} 
+
+// table - 2
+for (let i=2; i<=20; i=i+2) {
+    console.log(i);
+}
+
+// table - 3
+
+for (let i=3; i<=30; i=i+3) {
+    console.log(i);
+}
+
+// table - 17
+
+for (let i=17; i<=170; i=i+17) {
+    console.log(i);
+}
+
+// table - 19
+
+for (let i=19; i<=190; i=i+19) {
+    console.log(i);
+}
+
+// infinite loop
+
+// for (let i=1; i>=0; i++) {
+//     console.log(i);
+// }
+
+for (let i=5; i<=5555; i=i+10) {
+    console.log(i);
+}
