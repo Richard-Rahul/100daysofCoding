4951bb5694dfa7be324a9405655866c80768c9f4
Richard Rahul <mr.thesaga@gmail.com>
2024-04-07 13:51:32 +0530

    code updates

--- Day-78/app.js
+++ Day-78/app.js
@@ -1,3 +1,5 @@
+// Example - 1
+
 function myFunc(theArr) {
     theArr[0] = 30;
   }
@@ -7,3 +9,11 @@ const arr = [45];
 myFunc(arr);
 console.log(arr[0]); 
 console.log(arr[0]);
+
+// Example - 2
+
+function bark() {
+    console.log("Wolf");
+}
+
+bark.animal = "Dog";
---
+++ Day-79/app.js
@@ -0,0 +1,18 @@
+//Example - 1
+let heros = [["ironman", "spiderman", "thor", "starlord"], ["superman", "aquaman", "flash"]];
+
+for (let i =0; i<heros.length; i++) {
+    console.log(`list #${1}`);
+    for (j=0; j<heros[i].length; j++) {
+        console.log(heros[i][j]);
+    }
+}
+
+// Example - 2
+let student = [["aman", 96], ["rahul", 98], ["geeta", 99], ["aditya", 78]];
+
+for (let i=0; i<student.length; i++) {
+    for (let j=0; j<student[i].length; j++) {
+        console.log(student[i][j]);
+    }
+}
